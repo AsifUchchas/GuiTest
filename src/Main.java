fa0d5733413b53b2cd2217070dd4af467a09716a
AsifUchchas <asifuddinahmed123@gmail.com>
2021-09-10 20:59:00 +0600

    added scanner

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hi there!");
+
+        //Added a scanner object
+        String input;
+        try (Scanner scanner = new Scanner(System.in)) {
+            input = scanner.nextLine();
+        }
+
+
+        System.out.println(input);
     }
 }
